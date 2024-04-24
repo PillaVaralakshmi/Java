c265b31140b9c0856cfd936376cb64b1a513ff46
pillavaralakshmi <147524495+pillavaralakshmi@users.noreply.github.com>
2024-04-24 17:33:22 +0530

    Update ThreadExample.java
    update

--- ThreadExample.java
+++ ThreadExample.java
@@ -3,7 +3,7 @@
 
 class Multi extends Thread{  
 public void run(){  
-System.out.println("thread is running...");  
+System.out.println("thread is stopping...");  
 }  
 public static void main(String args[]){  
 Multi t1=new Multi();  
